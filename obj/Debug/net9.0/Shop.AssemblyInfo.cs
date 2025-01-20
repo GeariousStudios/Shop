414d5e44eb7e42a8a3236e2c0a864da10d0f61d5
GeariousStudios <gearious.studios@gmail.com>
2025-01-20 19:13:12 +0100

    Remove react again

--- obj/Debug/net9.0/Shop.AssemblyInfo.cs
+++ obj/Debug/net9.0/Shop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e0e9cb8f77d11a3b59bf5e85e16c1a48cef19f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73c5769103b47c94846fd38900932622e1d5a0f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
